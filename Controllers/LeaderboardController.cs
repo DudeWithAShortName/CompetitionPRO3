1574129679430839a8967d6cad88593d1fc9a030
DudeWithAShortName <stefan_kaloqn@abv.bg>
2022-03-26 09:39:21 +0200

    V14

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,20 @@
+﻿using Competition_PRO.Data;
+using Competition_PRO.Data.Models;
+using Competition_PRO.Models;
+using Competition_PRO.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Competition_PRO.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult Mechanic()
+        {
+            return this.View();
+        }
+    }
+}
